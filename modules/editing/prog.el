1c158dfb1e8e2057e223f2c73ad71b849f8d8abb
docktordreh <valentin_lechner@dismail.de>
2026-05-22 23:03:51 +0200

    Reset tooltip stipple for indent-bars

--- modules/editing/prog.el
+++ modules/editing/prog.el
@@ -7,6 +7,11 @@
 (use-package! indent-bars
   :config
   ;; Don’t draw guides behind docstrings.
-  (setq indent-bars-exclude-faces '(font-lock-doc-face)))
+  (setq indent-bars-exclude-faces '(font-lock-doc-face))
 
+  (defun +prog/reset-tooltip-stipple ()
+    (set-face-attribute 'tooltip nil :stipple nil))
+
+  (add-hook 'indent-bars-mode-hook #'+prog/reset-tooltip-stipple)
+  (add-hook 'after-enable-theme-hook #'+prog/reset-tooltip-stipple))
 
