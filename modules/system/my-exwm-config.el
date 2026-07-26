6480e5e33c57f483e86a81244713893306153997
docktordreh <valentin_lechner@dismail.de>
2026-07-26 19:54:48 +0200

    extend exwm

--- modules/system/my-exwm-config.el
+++ modules/system/my-exwm-config.el
@@ -1,13 +1,62 @@
 ;;; system/my-exwm-config.el -*- lexical-binding: t; -*-
 
 (require 'exwm)
-(setq exwm-workspace-number 4)
+(require 'cl-lib)
+(require 'evil)
+
+(setq exwm-workspace-number 5)
 (add-hook 'exwm-update-class-hook
-          (lambda() (exwm-workspace-rename-buffer exwm-class-name)))
+          (lambda () (exwm-workspace-rename-buffer exwm-class-name)))
+
+(defun my-exwm-start-process (name program &rest args)
+  "Start PROGRAM unless its EXWM session process is already running."
+  (let ((executable (executable-find program)))
+    (when (and executable (not (process-live-p (get-process name))))
+      (let ((process (apply #'start-process name nil executable args)))
+        (set-process-query-on-exit-flag process nil)))))
+
+(defun my-exwm-start-session-programs ()
+  "Start notification, authentication, tray, and clipboard utilities."
+  (make-directory (expand-file-name "parcellite" (or (getenv "XDG_DATA_HOME")
+                                                     "~/.local/share")) t)
+  (my-exwm-start-process "dunst" "dunst")
+  (my-exwm-start-process "polkit-mate-agent"
+                         "/usr/libexec/polkit-mate-authentication-agent-1")
+  (my-exwm-start-process "nm-applet" "nm-applet")
+  (my-exwm-start-process "blueman-applet" "blueman-applet")
+  (my-exwm-start-process "parcellite" "parcellite"))
+
+
+;; EXWM only sends these keys to Emacs in line mode when they are prefixes.
+;; Keep Doom's Evil leader and window commands ahead of the focused X client.
+(dolist (key '(32 ?\C-w))
+  (cl-pushnew key exwm-input-prefix-keys))
+(evil-set-initial-state 'exwm-mode 'normal)
 
 (setq exwm-input-global-keys
       `(([?\s-r] . exwm-reset)
         ([?\s-w] . exwm-workspace-switch)
+        (,(kbd "s-d") . (lambda ()
+                          (interactive)
+                          (my-exwm-start-process "session-launcher" "session-action" "launcher")))
+        (,(kbd "<print>") . (lambda ()
+                              (interactive)
+                              (my-exwm-start-process "session-screenshot" "session-action" "screenshot")))
+        (,(kbd "s-S") . (lambda ()
+                          (interactive)
+                          (my-exwm-start-process "session-screenshot" "session-action" "screenshot")))
+        (,(kbd "M-X") . (lambda ()
+                          (interactive)
+                          (my-exwm-start-process "session-lock" "session-action" "lock")))
+        (,(kbd "<XF86AudioPlay>") . (lambda ()
+                                      (interactive)
+                                      (my-exwm-start-process "playerctl-play" "playerctl" "play-pause")))
+        (,(kbd "<XF86AudioNext>") . (lambda ()
+                                      (interactive)
+                                      (my-exwm-start-process "playerctl-next" "playerctl" "next")))
+        (,(kbd "<XF86AudioPrev>") . (lambda ()
+                                      (interactive)
+                                      (my-exwm-start-process "playerctl-previous" "playerctl" "previous")))
         ([?\s-&] . (lambda (cmd)
                      (interactive (list (read-shell-command "$ ")))
                      (start-process-shell-command cmd nil cmd)))
@@ -19,12 +68,5 @@
                   (number-sequence 0 9))))
 (require 'exwm-systemtray)
 (exwm-systemtray-mode 1)
-(require 'exwm-randr)
-(setq exwm-randr-workspace-output-plist '(1 "eDP-1" 2 "DP-4-8"))
-(add-hook 'exwm-randr-screen-change-hook
-          (lambda ()
-            (start-process-shell-command
-             "xrandr" nil "xrandr --output eDP-1 --mode 2256x1504 --pos 0x658 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-4-8 --primary --mode 3840x2160 --pos 2256x0 --rotate normal --output DP-4-1 --off")))
-(exwm-randr-mode)
+(add-hook 'exwm-init-hook #'my-exwm-start-session-programs 90)
 (exwm-wm-mode)
-
--- modules/system/tty-exwm.el
+++ modules/system/tty-exwm.el
@@ -1,5 +1,5 @@
 ;;; system/tty-exwm.el -*- lexical-binding: t; -*-
 
 
-(when (not (display-graphic-p))
+(when (equal (getenv "EXWM") "1")
   (load! "my-exwm-config.el"))
