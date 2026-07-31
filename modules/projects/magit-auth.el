0b283aa838271d9007962cd3339ae87f821ce994
docktordreh <valentin_lechner@dismail.de>
2026-07-31 17:30:02 +0200

    authinfo.gpg

--- modules/projects/magit-auth.el
+++ modules/projects/magit-auth.el
@@ -1,7 +1,6 @@
 ;;; modules/projects/magit-auth.el -*- lexical-binding: t; -*-
 ;; Ensure Magit can retrieve credentials via auth-source.
 
-(setq auth-sources '("~/.authinfo.gpg")
+(setq auth-sources '("~/.config/auth/authinfo.gpg")
       auth-source-cache-expiry nil
       magit-process-find-password-functions '(magit-process-password-auth-source))
-
