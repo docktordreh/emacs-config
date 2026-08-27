3dc11e956dfd40c64c92035a2471307d76c1faad
docktordreh <valentin_lechner@dismail.de>
2026-08-27 18:02:53 +0200

    fix golden-ratio Evil window navigation

--- config.el
+++ config.el
@@ -16,6 +16,9 @@
 
 (use-package! golden-ratio
   :config
+  (dolist (command '(evil-window-left evil-window-right
+                     evil-window-up evil-window-down))
+    (add-to-list 'golden-ratio-extra-commands command))
   (golden-ratio-mode +1))
 
 (use-package! vertico-posframe
